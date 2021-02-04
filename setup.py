3702a34e9d295c8c57de5b22c994e4a1bba29e05
Nolan Taft <54606283+dev-nolant@users.noreply.github.com>
2021-02-03 22:36:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,23 +5,24 @@ with open('README.md') as readme_file:
 
 
 setup_args = dict(
-    name='Simple-LogHandler',
+    name='SHLogger',
     version='0.1.0',
     description='Super Simple Log handling system. With Error handling',
     long_description_content_type="text/markdown",
-    long_description=README + '\n\n' + HISTORY,
+    long_description=README,
     license='MIT',
     packages=find_packages(),
     author='Nolan Taft',
     author_email='contact@nolant.org',
     keywords=['simple-loghandler', 'SLH', 'Super simple'],
-    url='https://github.com/dev-nolant/simple-loghandler/',
+    url='https://github.com/dev-nolant/simple-loghandler',
     download_url='https://pypi.org/project/simple-loghandler/'
 )
 
 install_requires = [
     'datetime',
-    'os'
+    'os',
+    'wheel'
 ]
 
 if __name__ == '__main__':
